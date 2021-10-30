0dd6312a98f62923e411bf9f65c8e01bd2e67421
ashutosh887 <ashutoshj887@gmail.com>
2021-10-30 12:55:14 +0530

    basic route config

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// Route Imports
+const product = require("./routes/productRoute");
+
+// Routes Usage
+app.use("/api/v1", product);
+
 module.exports = app;
