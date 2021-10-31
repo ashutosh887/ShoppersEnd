46e60270b2889b9735b1474d43ac5d3d1a76460a
ashutosh887 <ashutoshj887@gmail.com>
2021-10-31 22:14:50 +0530

    Uncaught Exception error handled

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 const app = require("./app");
-
+const connectDatabase = require("./config/database");
 // Config Variables
 const dotenv = require("dotenv");
 dotenv.config({ path: "backend/config/config.env" });
 
-// Database Connectivity
-const connectDatabase = require("./config/database");
--connectDatabase();
+// Uncaught Exception Handling
+process.on("uncaughtException", (err) => {
+  console.log(`Error caught : ${err.message}`);
+  console.log("Server closed due to Uncaught Exception");
 
-app.listen(process.env.PORT, () => {
+  process.exit(1);
+});
+
+// Database Connectivity
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server started at PORT ${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error Occured : ${err.message}`);
+  console.log(`Shutting Down the Server due to Unhandled Promise Rejection.`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
