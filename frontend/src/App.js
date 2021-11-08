04b3b61d4483f76a76f3cc315d660fc7ee3a3f65
ashutosh887 <ashutoshj887@gmail.com>
2021-11-08 16:31:38 +0530

    UI Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Header from "./component/layout/Header/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <p>Ashutosh Jha's E Commerce App</p>;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
