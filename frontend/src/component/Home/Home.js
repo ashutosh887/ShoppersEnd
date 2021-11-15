5df28926d2ae7512a263f0220e2ab6b9b00f0eca
ashutosh887 <ashutoshj887@gmail.com>
2021-11-15 21:40:34 +0530

    alert added

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -7,51 +7,54 @@ import Product from "./Product.js";
 import Metadata from "../layout/Metadata";
 import { getProduct } from "../../actions/productAction";
 
-const product = {
-  name: "Blue Tshirt",
-  images: [
-    {
-      url: "https://images.unsplash.com/photo-1564859227552-81fde4a1df0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHQlMjBzaGlydHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    },
-  ],
-  price: "₹1000",
-  _id: "tshirt1000",
-};
+import { useAlert } from "react-alert";
+
+import Loader from "../layout/Loader/Loader";
 
 const Home = () => {
+  const alert = useAlert();
+
   const dispatch = useDispatch();
 
+  const { loading, error, products, productsCount } = useSelector(
+    (state) => state.products
+  );
+
   useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
     dispatch(getProduct());
-  }, [dispatch]);
+  }, [dispatch, error]);
 
   return (
     <Fragment>
-      <Metadata title={"Store 887"} />
-      <div className="banner">
-        <p>Welcome to store 887</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <Metadata title={"Store 887"} />
+          <div className="banner">
+            <p>Welcome to store 887</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-        <a href="#container">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
-      </div>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
 
-      <h2 className="homeHeading">Featured Products</h2>
+          <h2 className="homeHeading">Featured Products</h2>
 
-      <div className="container" id="container">
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-        <Product product={product} />
-      </div>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => {
+                return <Product product={product} />;
+              })}
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Store Configurations
 import { Provider } from "react-redux";
 import store from "./store";
 
+// Error Handling
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </Provider>,
   document.getElementById("root")
 );
